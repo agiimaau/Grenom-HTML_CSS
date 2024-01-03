01aa55b0a5349437ad24cdf93bc210bcd196c989
Agiimaa Unurtsolmon <104725247+agiimaau@users.noreply.github.com>
2024-01-03 14:43:46 +0800

    heh

---
+++ public/js/page-info.js
@@ -0,0 +1,17 @@
+const template = document.createElement("template");
+template.innerHTML = `
+
+
+
+            <slot class="info" name="info"></slot>
+    `;
+
+class PageInfo extends HTMLElement {
+  constructor() {
+    super();
+    const shadow = this.attachShadow({ mode: "open" });
+    shadow.appendChild(template.content.cloneNode(true));
+  }
+}
+
+window.customElements.define("page-info", ServiceInfo);
